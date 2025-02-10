546429cefe6f239795c96f1cfe75528c31720e4a
chrisshuo123 <chr1z100@gmail.com>
2025-02-11 03:53:53 +0700

    create table provinsi + describe / select provinsi table

--- query_dbKPU.sql
+++ query_dbKPU.sql
@@ -1,2 +1,13 @@
 /* Untuk mengganti schema DB yang digunakan */
 use pendataan_kpu;
+
+/* Table Wilayah */
+CREATE TABLE provinsi
+(
+	idProvinsi INT(4) NOT NULL AUTO_INCREMENT,
+    provinsi VARCHAR(30) NOT NULL,
+    PRIMARY KEY (idProvinsi)
+) ENGINE = InnoDB;
+
+select * from provinsi;
+describe provinsi;
